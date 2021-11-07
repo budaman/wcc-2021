12a873533cca41f20b144740d114956f31ea3028
Mantvydas <mantvydas@Mantvydass-MacBook-Pro.local>
2021-11-07 14:21:37 +0200

    created first animals component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,7 +1,35 @@
 import "./styles.css";
+import { loader } from "graphql.macro";
+import { useQuery } from "@apollo/client";
+
+const GET_ANIMALS_QUERY = loader("../../graphql/queries/animal-list.graphql");
 
 function App() {
-  return <div className="container">Wcc</div>;
+  const { loading, data, error } = useQuery(GET_ANIMALS_QUERY);
+
+  if (loading) {
+    return <h2>Loading....</h2>;
+  }
+
+  if (error) {
+    return <h2>Opppps.... Something went wrong</h2>;
+  }
+
+  return (
+    <div className="container">
+      {data.animals?.edges.map((animal) => (
+        <div key={animal.node.id}>
+          <p>{animal.node.name}</p>
+          <img
+            src={
+              animal.node.imageUrl ||
+              "https://i.insider.com/5ebbfc9ffc593d729d60df73?width=1136&format=jpeg"
+            }
+          />
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
