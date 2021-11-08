f6e6a6fecea9dc8577c6d13dbfb036a59a3b3c32
Mantvydas <mantvydas@Mantvydass-MacBook-Pro.local>
2021-11-08 19:27:52 +0200

    styles update

--- src/components/AnimalList/index.js
+++ src/components/AnimalList/index.js
@@ -34,7 +34,7 @@ const AnimalList = () => {
             height="140"
             image={
               animal.node.imageUrl ||
-              "https://i.insider.com/5ebbfc9ffc593d729d60df73?width=1136&format=jpeg"
+              "https://pro2-bar-s3-cdn-cf6.myportfolio.com/c728a553-9706-473c-adca-fa2ea3652db5/729b72ec-104f-4499-9986-0dbaf11ce437_rw_1200.jpg?h=6aea14fe1fe3fd0853669fd14184b6f7"
             }
           />
           <CardContent>
