256bd637f0116b4f53db3de2fec41bb50519e8f2
Mantvydas <budamantvydas@gmail.com>
2021-11-22 17:49:04 +0200

    step 2

--- src/components/AnimalList/index.js
+++ src/components/AnimalList/index.js
@@ -8,6 +8,10 @@ const GET_ANIMALS_QUERY = loader("../../graphql/queries/animal-list.graphql");
 const AnimalList = () => {
   const { loading, data, error } = useQuery(GET_ANIMALS_QUERY);
 
+  if (error) return <div>Oppps, something went wrong!</div>;
+
+  if (loading) return <div>Loading</div>;
+
   return <div className={styles.container}>Animal List komponentas!</div>;
 };
 
