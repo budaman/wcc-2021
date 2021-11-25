9f0c4a201e51243170b2ed8abb0ba9a7f7a59849
Mantvydas <budamantvydas@gmail.com>
2021-11-25 16:45:47 +0200

    animal card component

--- src/components/AnimalList/index.js
+++ src/components/AnimalList/index.js
@@ -2,15 +2,9 @@ import { useState } from "react";
 import { loader } from "graphql.macro";
 import { useQuery } from "@apollo/client";
 
-import { Link } from "react-router-dom";
+import { LinearProgress, TextField } from "@mui/material";
 
-import {
-  LinearProgress,
-  Card,
-  CardMedia,
-  CardContent,
-  TextField,
-} from "@mui/material";
+import AnimalCard from "../AnimalCard";
 
 import SearchIcon from "@mui/icons-material/Search";
 import CloseIcon from "@mui/icons-material/Close";
@@ -18,8 +12,6 @@ import CloseIcon from "@mui/icons-material/Close";
 import styles from "./styles.module.css";
 
 const GET_ANIMALS_QUERY = loader("../../graphql/queries/animal-list.graphql");
-const DEFAULT_ANIMAL_IMAGE =
-  "https://pro2-bar-s3-cdn-cf6.myportfolio.com/c728a553-9706-473c-adca-fa2ea3652db5/729b72ec-104f-4499-9986-0dbaf11ce437_rw_1200.jpg?h=6aea14fe1fe3fd0853669fd14184b6f7";
 
 const AnimalList = () => {
   const { data, loading, error } = useQuery(GET_ANIMALS_QUERY);
@@ -66,19 +58,11 @@ const AnimalList = () => {
       />
       <div className={styles.animalContainer}>
         {filterAnimals().map((animal) => (
-          <Link key={animal.node.id} to={`animal/${animal.node.id}`}>
-            <Card sx={{ width: 300 }} className={styles.cardContainer}>
-              <CardMedia
-                component="img"
-                alt="green iguana"
-                height="140"
-                image={animal.node.imageUrl || DEFAULT_ANIMAL_IMAGE}
-              />
-              <CardContent>
-                <p>{animal.node.name}</p>
-              </CardContent>
-            </Card>
-          </Link>
+          <AnimalCard
+            id={animal.node.id}
+            name={animal.node.name}
+            image={animal.node.imageUrl}
+          />
         ))}
       </div>
     </div>
