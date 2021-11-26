112adb0f34c132fda1100eef3153c21ead31d3d8
Mantvydas <budamantvydas@gmail.com>
2021-11-26 14:28:19 +0200

    start

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,25 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-import styles from "./styles.module.css";
-
-import AnimalList from "../AnimalList";
-import AnimalProfile from "../AnimalProfile";
-import About from "../About";
-import Header from "../Header";
+import "./styles.css";
 
 const App = () => {
-  return (
-    <Router>
-      <div className={styles.container}>
-        <Header />
-        <Routes>
-          <Route path="/" element={<AnimalList />} />
-          <Route path="/animal/:id" element={<AnimalProfile />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+  return <div className="container">Pet Book App</div>;
 };
 
 export default App;
